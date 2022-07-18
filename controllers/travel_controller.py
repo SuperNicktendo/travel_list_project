50877761c5b15c91418f12f1041f09b26a1ffa18
Nick Murray <nicholas.murray1332@googlemail.com>
2022-07-18 18:39:21 +0100

    altered repositories

--- controllers/travel_controller.py
+++ controllers/travel_controller.py
@@ -7,6 +7,9 @@ from models.city import City
 from models.country import Country
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
+import repositories.user_repository as user_repository
+import repositories.visited_repository as user_repository
+
 
 travel_blueprint = Blueprint("travel", __name__)
 
---
+++ repositories/wishlist_repository.py
@@ -0,0 +1,5 @@
+from db.run_sql import run_sql
+from models.visited import Visit
+from models.city import City
+from models.country import Country
+from models.user import User
